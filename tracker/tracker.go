bf29ff5b4c7509bd546d9e351f3e83d81569247b
cchen-byte <chenzhiwen@channelwill.cn>
2022-07-13 22:34:41 +0800

    fix:logic

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,9 +1,12 @@
 package tracker
 
-import "github.com/cchen-byte/trackeSharkes/httpobj"
+import (
+	"github.com/cchen-byte/trackeSharkes/httpobj"
+	"github.com/cchen-byte/trackeSharkes/middleware"
+)
 
 type Tracker interface {
 	fetchWork(request *httpobj.Request) (*httpobj.Response, error)
 	parseWork(request *httpobj.Request, resp *httpobj.Response) (*httpobj.ParseResult, error)
-	Run()
+	Run(trackerMiddlewaresManager *middleware.Manager, request *httpobj.Request) (*httpobj.ParseResult, error)
 }
